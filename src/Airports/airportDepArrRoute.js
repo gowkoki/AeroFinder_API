c5d76f7f31124e7213eb493cfa7c5ae83422c77c
gowkoki <91251816+gowkoki@users.noreply.github.com>
2024-05-17 11:41:13 +0100

    Update airportDepArrRoute.js
    Fixing the bug

--- src/Airports/airportDepArrRoute.js
+++ src/Airports/airportDepArrRoute.js
@@ -10,9 +10,11 @@ router.get("/airport/departure/arrival", async (req, res) => {
 
   // Get the current date and time in the specified format
   const currentDateTime = moment().format("YYYY-MM-DDTHH:mm");
+  console.log(currentDateTime);
 
   // Calculate the end date and time (current date and time + 12 hours)
   const endDateTime = moment().add(12, "hours").format("YYYY-MM-DDTHH:mm");
+  console.log(endDateTime);
 
   // Define API request options for fetching flights
   const options = {
